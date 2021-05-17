25da1b49eaaaf993216a7786eebd6e24c8b668a4
ion_u <mmmariciuc223@gmail.com>
2021-05-17 05:00:35 +0300

    Update backend.

--- app/src/main/java/com/caramelpanda/driversapp/MainActivity.java
+++ app/src/main/java/com/caramelpanda/driversapp/MainActivity.java
@@ -1,21 +1,34 @@
 package com.caramelpanda.driversapp;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.caramelpanda.driversapp.data.Account;
 import com.caramelpanda.driversapp.ui.main.SectionsPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    static Account account;
+
+    public static Account getAccount() {
+        return account;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
+        account = (Account) getIntent().getSerializableExtra("user");
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
--- app/src/main/java/com/caramelpanda/driversapp/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/caramelpanda/driversapp/ui/main/SectionsPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.caramelpanda.driversapp.ui.main;
 
 import android.content.Context;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import com.caramelpanda.driversapp.MainActivity;
 import com.caramelpanda.driversapp.R;
 import com.caramelpanda.driversapp.ui.main.tabs.MapsFragment;
 import com.caramelpanda.driversapp.ui.main.tabs.SettingsFragment;
@@ -36,6 +38,10 @@ public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
         switch (position) {
             case 0:
                 fragment = new UserFragment();
+
+                Bundle b = new Bundle();
+                b.putSerializable("user", MainActivity.getAccount());
+                fragment.setArguments(b);
                 break;
             case 1:
                 fragment = new MapsFragment();
